4582ab88d645a6509943e5cf5fc98fbd4dc83d00
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-10-05 14:46:21 +0300

    :bug:

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function () {
     $('#popup-close, #popup-background').click(function () {
-        $('#popup-background, #popup-content').addClass('d-none')
+        $('#popup-background, #popup-content, #popup-question').addClass('d-none')
     })
 })
