f0ffdf009cd55cd505ba9335741c84c5ebb639da
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-11-25 13:08:06 +0200

    test es8

---
+++ functions/es8search.js
@@ -0,0 +1,42 @@
+const https = require('https')
+
+
+exports.handler = (event, context, callback) => {
+    const options = {
+        hostname: 'd3869450b84047e98abb54ff625681cc.eu-central-1.aws.cloud.es.io',
+        port: 9243,
+        path: '/emem_persons/_search?',
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic cmVhZGVyOnJlYWRlcg==',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const request = https.request(options, response => {
+        var body = ''
+
+        response.on('data', function (d) {
+            body += d
+        })
+
+        response.on('end', function () {
+            callback(null, {
+                statusCode: 200,
+                headers: { 'Content-Type': 'application/json' },
+                body: body
+            })
+        })
+    })
+
+    request.on('error', function () {
+        callback(null, {
+            statusCode: 500,
+            headers: { 'Content-Type': 'application/json' },
+            body: body
+        })
+    })
+
+    request.write(event.body)
+    request.end()
+}
