6778809494b57202a0e35beec63b09072b33cc97
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-11-28 11:10:05 +0200

    authorize as readonly user

--- functions/search.js
+++ functions/search.js
@@ -8,7 +8,7 @@ exports.handler = (event, context, callback) => {
         path: '/emem_persons/_search?',
         method: 'POST',
         headers: {
-            'Authorization': 'Basic cmVhZGVyOnJlYWRlcg==',
+            'Authorization': 'Basic cmVhZGVyOkZpdWg4V2trVGR4MjY0Ugo=',
             'Content-Type': 'application/json'
         }
     }
