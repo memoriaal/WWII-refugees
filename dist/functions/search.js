51a0d6cff5340c4b930e16e3e0275dc228d13e5b
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-09-19 12:16:00 +0300

    try#4

--- dist/functions/search.js
+++
@@ -1,9 +0,0 @@
-exports.handler = (event, context, callback) => {
-
-    callback(null, {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: event.body
-    })
-
-}
